44925f39c6f0e803f8a25c333ec6f1321965e2e5
david-levi-student <dal0531@my.londonmet.ac.uk>
2022-03-22 16:12:21 +0000

    Adding menu

--- Learning SDL/MenuState.cpp
+++ Learning SDL/MenuState.cpp
@@ -1,1 +1,31 @@
 #include "MenuState.h"
+
+bool MenuState::OnEnter()
+{
+	return true;
+}
+
+State* MenuState::Update()
+{
+	if (Input::Instance()->ClosedWindow())
+	{
+		return nullptr;
+	}
+
+	if (Input::Instance()->GetKeyDown() == SDLK_ESCAPE)
+	{
+		return nullptr;
+	}
+	
+	return this;
+}
+
+bool MenuState::Render()
+{
+	return true;
+}
+
+void MenuState::OnExit()
+{
+
+}
--- Learning SDL/MenuState.h
+++ Learning SDL/MenuState.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "State.h"
+#include "Input.h"
+
 class MenuState : public State
 {
+	
+public:
 
+	MenuState() {}
+	~MenuState() override {}
+
+	bool OnEnter() override;
+	State* Update() override;
+	bool Render() override;
+	void OnExit() override;
 };
